3bd8c2310701b463285d50189f81c55a7b0c7882
Huyhehe <thhyy2002@gmail.com>
2022-09-06 23:24:35 +0700

    move header from ProfileLayout to Profile page file and add profile-navbar

--- src/Layouts/ProfileLayout/index.jsx
+++ src/Layouts/ProfileLayout/index.jsx
@@ -1,29 +1,5 @@
-import { MdKeyboardArrowUp } from "react-icons/md";
-import { useSelector } from "react-redux";
-
 const ProfileLayout = ({ children }) => {
-  const user = useSelector((state) => state.user?.user);
-  return (
-    <div>
-      <div className="profile-header flex justify-between">
-        <div className="profile-header__bgBar h-[90px] w-full bg-black relative px-[5rem] flex items-end gap-4">
-          <div className="profile-header__avatar relative top-[100%] w-[170px] rounded-full aspect-square bg-slate-100"></div>
-          <div className="text-white">
-            <span className="text-[1.25rem]">{user?.name}</span>
-          </div>
-          <div className="profile-header__toolbox ml-auto">
-            <div className="group flex items-center text-white">
-              <span className="text-[1.5em]">{user?.userName}</span>
-              <div className="group-hover:rotate-180 transition-all duration-300">
-                <MdKeyboardArrowUp size={"1.5em"} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>{children}</div>
-    </div>
-  );
+  return <div>{children}</div>;
 };
 
 export default ProfileLayout;
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,7 +1,46 @@
+import { MdKeyboardArrowUp } from "react-icons/md";
+import { useSelector } from "react-redux";
+
 const Profile = () => {
+  const user = useSelector((state) => state.user?.user);
+  const links = [
+    {
+      title: "Information",
+      direction: "/profile/info",
+    },
+    {
+      title: "My blogs",
+      direction: "/profile/blogs",
+    },
+  ];
+
   return (
-    <div>
-      
+    <div className="profile-container">
+      <div className="profile-header flex justify-between mb-[10rem]">
+        <div className="profile-header__bgBar h-[90px] w-full bg-black relative px-[5rem] flex items-end gap-4">
+          <div className="profile-header__avatar relative top-[100%] w-[170px] rounded-full aspect-square bg-slate-100"></div>
+          <div className="text-white">
+            <span className="text-[1.25rem]">{user?.name}</span>
+          </div>
+          <div className="profile-header__toolbox ml-auto">
+            <div className="group flex items-center text-white">
+              <span className="text-[1.5em]">{user?.userName}</span>
+              <div className="group-hover:rotate-180 transition-all duration-300">
+                <MdKeyboardArrowUp size={"1.5em"} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="profile-navbar flex gap-[1rem] px-[5rem]">
+        {links.map((link, index) => {
+          return (
+            <div key={index} className="cursor-pointer">
+              <span className="text-[1.75rem] font-medium">{link.title}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
